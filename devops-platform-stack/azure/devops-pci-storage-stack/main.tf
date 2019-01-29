419ae6b0228cb23d4350e4e43a76a73faaccf983
vishu029 <47076708+vishu029@users.noreply.github.com>
2019-01-29 17:48:03 +0530

    Update main.tf

--- devops-platform-stack/azure/devops-pci-storage-stack/main.tf
+++ devops-platform-stack/azure/devops-pci-storage-stack/main.tf
@@ -2,14 +2,14 @@
 
 # Creating a pci resource group for associating further resources with it.
 module "resource_group" {
-  source   = "git::https://github.com/vishu029/dev.git"
+  source   = "git::https://github.com/vishu029/dev.git//devops-module-stack/azure/common/resource-group"
   name     = "devops-${var.environment}-${var.region}-${var.resource_group_name}"
   location = "${var.location}"
 }
 
 # Creating a storage group which will create a storage account and storage containers
 module "storage_group" {
-  source                         = "git::https://github.com/vishu029/dev.git"
+  source                         = "git::https://github.com/vishu029/dev.git//devops-module-stack/azure/storage"
   storage_name                   = "${var.environment}${var.region}${var.storage_account_name}"
   location                       = "${var.location}"
   resource_group_name            = "${module.resource_group.name}"
